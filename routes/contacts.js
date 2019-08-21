d5d1e65b52ff8a4b67f9b81d2d4a1d29bccdaa67
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-21 11:30:23 -0300

    CRUD contatos

--- routes/contacts.js
+++ routes/contacts.js
@@ -26,22 +26,106 @@ router.get("/", auth, async (req, res) => {
 //@rota     POST api/contacts
 //@desc     Adiciona novo contato
 //@acesso   Privado
-router.post("/", (req, res) => {
-  res.send("Adiciona novo contato para o usuário logado");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "Por favor, adicione um nome ao contato")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //verifica se há erros na validação
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Erro no Servidor");
+    }
+  }
+);
 
 //@rota     PUT api/contacts/:id
 //@desc     Atualiza contato
 //acesso    Privado
-router.put("/:id", (req, res) => {
-  res.send("Atualiza o contato");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  //verifica campos adicionados e adiciona no objeto caso existam
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.name = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact)
+      return res.status(404).json({ msg: "Contato não encontrado" });
+
+    //verifica se usuário logado detém o contato
+    if (contact.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: "Você não tem permissão para alterar este contato!" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no Servidor");
+  }
 });
 
 //@rota     DELETE api/contacts/:id
 //@desc     remove contato
 //@acesso   Privado
-router.delete("/:id", (req, res) => {
-  res.send("Apaga o contato");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact)
+      return res.status(404).json({ msg: "Contato não encontrado" });
+
+    //verifica se usuário logado detém o contato
+    if (contact.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: "Você não tem permissão para alterar este contato!" });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Contato removido" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no Servidor");
+  }
 });
 
 module.exports = router;
