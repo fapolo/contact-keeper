cdc363d4c4e2dd1bcec41a6122b8a81ace56916d
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-21 09:46:11 -0300

    add cadastro simples - user no db

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
 //@route    POST api/users
-//@desc     Register a user
+//@desc     Registra usuário
 //@access   Public
 router.post(
   "/",
+  //middleware - define campos obrigatórios express-validator
   [
     check("name", "Por favor, adicione o nome corretamente.")
       .not()
@@ -20,13 +22,41 @@ router.post(
       "Por favor, adicione uma senha com no mínimo 6 caracteres."
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
+    //verifica se há erros na validação
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("Validação OK");
+    const { name, email, password } = req.body;
+
+    //busca se usuário já existe no db
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "Usuário já cadastrado" });
+      }
+
+      //cria usuário - pwd encriptado com bcrypt
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("Usuário cadastrado");
+    } catch (err) {
+      console.error(err.message);
+      res.status(400).send("Erro no Servidor");
+    }
   }
 );
 
