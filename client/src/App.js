4b208547831e94de8af80ea5df22498470ccecfc
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-22 09:59:10 -0300

    Estrutura pages home e about

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+
 import "./App.css";
 
 const App = () => {
-  return <div className='App'>My App</div>;
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Sobre esta aplicação</h1>
+      <p className='my-1'>Aplicação React Full Stack para controlar contatos</p>
+      <p className='bg-dark p'>
+        <strong>Versão</strong>: 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
