6ee401e36b2a4d6f61a61cfa7f8d05f2ab6768c0
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-27 08:48:49 -0300

    filtrar contatos

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = props => {
         type: "profissional"
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -70,19 +71,28 @@ const ContactState = props => {
   };
 
   // FILTRAR CONTATOS
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   // LIMPAR FILTRO
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
