2c4d7ed209987013ac31c52bece39f856701b39c
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-28 10:42:58 -0300

    atualiza pagina sobre

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,10 +4,28 @@ const About = () => {
   return (
     <div>
       <h1>Sobre esta aplicação</h1>
-      <p className='my-1'>Aplicação React Full Stack para controlar contatos</p>
+      <p className='my-1'>
+        Aplicação React Full Stack para controlar contatos. Resultado de curso
+        para aprendizado de React.
+      </p>
+      <p className='my-1'>Utiliza as seguintes tecnologias:</p>
+      <ul className='list'>
+        <li>NodeJS</li>
+        <li>Express</li>
+        <li>MongoDB (Atlas)</li>
+        <li>JSON Web Token</li>
+        <li>React</li>
+        <li>React Transition Group</li>
+        <li>React Hooks</li>
+        <li>React Context</li>
+      </ul>
       <p className='bg-dark p'>
         <strong>Versão</strong>: 1.0.0
       </p>
+      <strong>Repositório</strong>:{" "}
+      <a href='https://github.com/fapolo/contact-keeper' target='_blank'>
+        Clique Aqui{" "}
+      </a>
     </div>
   );
 };
