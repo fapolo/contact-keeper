7192fff4952ba89eb0fb20a57a00e1f1a38ebcc2
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-28 10:54:00 -0300

    heroku deploy settings

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const usersRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -10,11 +11,19 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "Hello World" }));
-
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/contacts", contactsRoute);
+
+// Static Assest for production
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
